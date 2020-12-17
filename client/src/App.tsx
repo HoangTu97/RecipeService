96547a30f98529c6dc5548f8d0fe86760be644ff
HoangTuNX <tu.nhx@big-labs.com>
2020-12-17 09:18:12 +0700

    add example UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import SignIn from "recipe-pages/home";
+import "./App.css";
+
+const theme = createMuiTheme();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <SignIn />
+    </MuiThemeProvider>
   );
 }
 
---
+++ client/src/pages/news_feed/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import MasterLayout from "../layouts/master";
+
+export default function NewsFeed() {
+  return (
+    <MasterLayout>
+      <p>news feed</p>
+    </MasterLayout>
+  );
+}
